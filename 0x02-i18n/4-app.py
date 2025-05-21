0961756a06e646d9bddf81dabdc346b67bc8fc50
Maurice Ngicho <mrcngicho@gmail.com>
2025-05-21 17:32:52 +0300

    Force locale with URL parameter

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""Flask app with basic Babel setup"""
+
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, _, get_locale
+
+
+class Config:
+    """App configuration class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+# Avail get_locale to Jinja templates
+app.jinja_env.globals['get_locale'] = get_locale
+
+
+@babel.localeselector
+def get_locale():
+    """Determine the best match with supported languages"""
+    user_locale = request.args.get('locale')
+    if user_locale and user_locale in app.config['LANGUAGES']:
+        return user_locale
+    return (request.accept_languages.best_match(app.config['LANGUAGES']))
+
+
+@app.route('/')
+def index():
+    """Render the index page"""
+    return (render_template('3-index.html'))
+
+
+if __name__ == '__main__':
+    app.run()
